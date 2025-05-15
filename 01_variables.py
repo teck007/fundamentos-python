535fd4133320f4dcffb78be607746b464d42e67b
Axel Pincheira <teck007@gmail.com>
2025-05-15 17:27:31 -0400

    ejemplo variables

---
+++ 01_variables.py
@@ -0,0 +1,19 @@
+#Declaración de variables
+nombre = "Axel"
+edad = 37
+curso = "4 Programación"
+sexo = "masculino"
+
+#mostrar datos
+print(nombre, edad, curso, sexo)
+
+#mostrar todos los datos en un solo print
+print("Mi nombre es:", nombre, "mi edad es", edad, "mi sexo es", sexo, "Soy profesor del", curso)
+
+#print en dos lineas
+print("Mi nombre es:", nombre, "mi edad es", edad, 
+      "mi sexo es", sexo, "Soy profesor del", curso)
+
+#Recibir información
+nombre = input("Ingrese nombre")
+print(nombre)
