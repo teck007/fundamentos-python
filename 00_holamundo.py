3e8b68a09c6563a9fa0b086a0c96036728cef289
Axel Pincheira <teck007@gmail.com>
2025-05-15 15:55:33 -0400

    archivos iniciales

---
+++ 00_holamundo.py
@@ -0,0 +1,21 @@
+# HOLA MUNDO
+"""
+    HOLA
+    MUNDO
+"""
+'''
+    HOLA
+    MUNDO
+'''
+#Se muestra texto en pantalla
+print("HOLA")
+print('MUNDO')
+
+#Se muestra un número
+print(5)
+
+#Se muestra un número con decimales
+print(1.5)
+
+#Se muestra un texto y un número
+print("Mi edad es", 37)
