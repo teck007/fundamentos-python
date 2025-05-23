d1c5e3fba74f0b62e888ee769c1c0494cc439cb4
Axel Pincheira <teck007@gmail.com>
2025-05-23 09:06:47 -0400

    se cambian algunos nombres

---
+++ Ejercicios/01_imc.py
@@ -0,0 +1,7 @@
+#Ejercicio N1
+#calculadora de IMC
+#peso/(altura²)
+
+peso = float(input("Ingrese peso en kilos"))
+altura = float(input("Ingrese altura en metros"))
+print("Su IMC es:", peso/altura**2)
