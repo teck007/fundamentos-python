25dd9314689105f3b3eb967265bcb4b39bb10230
Patricio <patriciorenault@gmail.comm>
2025-05-26 20:22:35 -0400

    Agrego el avance de 04-listas

---
+++ 04_list.py
@@ -0,0 +1,10 @@
+### Definicion de listas ###    
+
+mi_lista = list() # lista vacia
+mi_otra_lista =[] # lista vacia
+
+print(len(mi_lista)) # longitud de la lista
+
+mi_lista = [38, 17, 17, 17, 18] # lista con elementos
+
+print(mi_lista) # imprime la lista
