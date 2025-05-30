ff01d67f09f1ac0e7250858d601d84fe2ad208d5
Axel Pincheira <teck007@gmail.com>
2025-05-30 10:00:40 -0400

    Se agrega diccionarios

--- 05_diccionarios.py
+++ 05_diccionarios.py
@@ -1,4 +1,17 @@
-#Definir un diccionario
+#Diccionarios
+#Definir diccionario
+datos = {}
+datos = {"nombre": "Axel", "apellido": "Pincheira", "edad": 37}
 
-alumnos = {"nombre:": "Axel", "apellido": "Pincheira", "edad": 37}
-print(alumnos)
+#Mostrar diccionario
+print(datos)
+
+#Mostrar un dato en concreto
+print(datos["nombre"])
+
+#Ingresar un dato
+datos["sexo"]="masculino"
+
+#Ingresa un dato y verifica si existe la clave
+datos.setdefault("sexo", "femenino")
+print(datos)
