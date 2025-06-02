e88f47b514e86305d62197228ae6e5f06de2eba9
Axel Pincheira <teck007@gmail.com>
2025-06-02 09:57:53 -0400

    se agrega ejercicio de diccionario

---
+++ Ejercicios/10_datos_personales.py
@@ -0,0 +1,11 @@
+"""
+Crear el siguiente programa en Python utilizando un diccionario
+1. El usuario debe ingresar nombre, edad, profesion, tecnologías aprendidas
+2. Guardar los datos en un diccionario
+3. Borrar edad
+4. Mostrar todos los valores del diccionario
+5. Mostrar los datos con un mensaje
+"""
+# Pedir datos al usuario
+datos_personales = {}
+tecnologias = input("Ingrese las tecnologias separadas por comas: ").split(",")
