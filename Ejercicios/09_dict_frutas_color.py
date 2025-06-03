cf05c72a676dad67e249606a0cc7a64523a57fd5
Patricio <patriciorenault@gmail.comm>
2025-06-03 08:42:26 -0400

    diccionario de frutas

---
+++ Ejercicios/09_dict_frutas_color.py
@@ -0,0 +1,17 @@
+"""
+Ejercicio: Crear un diccionario de frutas y colores
+
+Crea un diccionario vacío llamado frutas_colores.
+
+Agrega 6 frutas a ese diccionario con sus respectivos colores. 
+Por ejemplo:
+
+"manzana": "rojo"
+
+"banana": "amarillo"
+
+"kiwi": "verde"
+
+Imprime el diccionario completo para verificar que los datos 
+se han agregado correctamente.
+"""
