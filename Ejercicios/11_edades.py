c56244d226a40d34578e5d900145608a0b9726e3
Axel Pincheira <teck007@gmail.com>
2025-06-05 18:23:15 -0400

    se agrega ejercicio

---
+++ Ejercicios/11_edades.py
@@ -0,0 +1,14 @@
+"""
+Ejercicio N11
+1. Pedir al usuario su año de nacimiento (input).
+2. Definir año actual en una variable .
+3. Calcular la edad restando el año de nacimiento al año actual.
+4. Determinar el rango etario usando condicionales:
+   - Si la edad está entre 1 y 17: "Menor de edad"
+   - Si la edad está entre 18 y 25: "Adulto joven"
+   - Si la edad está entre 26 y 59: "Adulto"
+   - Si la edad está entre 60 y 100: "Adulto mayor"
+5. Mostrar al usuario su edad y rango etario (print).
+Utilizar condicionales, mayor o igual >=     menor o igual <=
+"""
+fecha_nac = int(input("Ingrese fecha de nacimiento"))
