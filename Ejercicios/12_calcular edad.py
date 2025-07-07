30fe379fdb8b40dc639ab0855145b1ec074d13c4
Axel Pincheira <teck007@gmail.com>
2025-07-07 09:51:24 -0400

    algunas correcciones y archivo de repaso

--- Ejercicios/12_calcular edad.py
+++
@@ -1,6 +0,0 @@
-# Ejercicio 11: Calcular la edad
-# Crear un programa que verifique si la edad de una persona es mayor o menor que 18 años.
-# # 1. Pedir al usuario que ingrese su edad.
-# edad = int(input("Ingrese su edad: "))
-# # 2. Si la edad es mayor o igual a 18, mostrar un mensaje indicando que es mayor de edad. 
-# # Si es menor, mostrar un mensaje indicando que es menor de edad. 
---
+++ repaso.py
@@ -0,0 +1,4 @@
+nac = 2007
+nombre = "Joaquin"
+edad = (2025 - nac) - 1
+print("Mi nombre es:", nombre, "y mi edad es:", edad)
