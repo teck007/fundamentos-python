666b38651c0fc5635bf834369c9fd59124ca0cae
Axel Pincheira <teck007@gmail.com>
2025-07-07 08:51:44 -0400

    Se agrega respuesta de ejercicio

--- Ejercicios/11_edades.py
+++ Ejercicios/11_edades.py
@@ -11,4 +11,19 @@ Ejercicio N11
 5. Mostrar al usuario su edad y rango etario (print).
 Utilizar condicionales, mayor o igual >=     menor o igual <=
 """
-fecha_nac = int(input("Ingrese fecha de nacimiento"))
+fecha_nac = int(input("Ingrese su año de nacimiento: "))
+anio_actual = 2025
+edad = anio_actual - fecha_nac
+
+if 1 <= edad <= 17:
+    rango = "Menor de edad"
+elif 18 <= edad <= 25:
+    rango = "Adulto joven"
+elif 26 <= edad <= 59:
+    rango = "Adulto"
+elif 60 <= edad <= 100:
+    rango = "Adulto mayor"
+else:
+    rango = "Edad fuera de rango"
+
+print(f"Tienes {edad} años. Rango etario: {rango}.")
