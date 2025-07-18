d1f321bb986ca229204d29cc3fd8faaa5fc46919
Axel Pincheira <teck007@gmail.com>
2025-07-18 09:26:39 -0400

    se agrega desarrollo de ejercicio

--- funciones2.py
+++ funciones2.py
@@ -1,10 +1,10 @@
 # ejercicio 1
 def multiplicax2(num1):
     resultado = []
-    for i in range(num1):
-      resultado.append(input("Ingrese nombre" + str(i)))
+    for i in range(num1+1):
+      resultado.append(i*2)
     return resultado  
-cantidad = int(input("cantidad de personas"))
-print(multiplicax2(cantidad))
+
+print(multiplicax2(5))
 
 # ejercicio 2
