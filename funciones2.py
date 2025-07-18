120c12831aa2aecea49d5be6cc283886b483fa1e
Axel Pincheira <teck007@gmail.com>
2025-07-18 09:24:42 -0400

    se agrega repaso de funciones

---
+++ funciones2.py
@@ -0,0 +1,10 @@
+# ejercicio 1
+def multiplicax2(num1):
+    resultado = []
+    for i in range(num1):
+      resultado.append(input("Ingrese nombre" + str(i)))
+    return resultado  
+cantidad = int(input("cantidad de personas"))
+print(multiplicax2(cantidad))
+
+# ejercicio 2
